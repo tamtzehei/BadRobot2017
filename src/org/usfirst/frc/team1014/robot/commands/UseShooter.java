f9d4e475f4f46dd01cfb7e74263ee4ac6ad1b6fe
roryg_000 <roryg_000@10.81.20.194>
2017-02-17 17:20:58 -0500

    add using gather and shooter

--- src/org/usfirst/frc/team1014/robot/commands/UseShooter.java
+++ src/org/usfirst/frc/team1014/robot/commands/UseShooter.java
@@ -3,29 +3,51 @@ package org.usfirst.frc.team1014.robot.commands;
 import org.usfirst.frc.team1014.robot.OI;
 import org.usfirst.frc.team1014.robot.subsystems.Shooter;
 
-import edu.wpi.first.wpilibj.command.Command;
+import edu.wpi.first.wpilibj.command.Subsystem;
 
-public class UseShooter extends Command {
+public class UseShooter extends CommandBase {
 
-	Shooter shooter;
 	public static double speed;
+	public static double spinnerSpeed;
 
+	public UseShooter()
+	{
+		requires((Subsystem) shooter);
+	}
+	
+	/*
 	public UseShooter() {
 		requires(shooter.getInstance());
 		speed = 1;
+	}*/
+	
+	protected void initialize() {
+		shooter.shoot(0.0);
+		shooter.rotateSpinner(0.0);
 	}
 
 	protected void execute() {
-		if (OI.xboxController0.getYButton()) {
-			shooter.rotateFeeder(speed);
+		if (OI.xboxController0.getYButton()) 
+		{
 			shooter.shoot(speed);
 		}
+		shooter.rotateSpinner(spinnerSpeed);
 	}
 
 	@Override
 	protected boolean isFinished() {
-		// TODO Auto-generated method stub
 		return false;
+	}
+
+	protected void end()
+	{
+		shooter.shoot(0.0);
+	}
+	
+	
+	public String getConsoleIdentity() {
+		// TODO Auto-generated method stub
+		return "Use_Shooter";
 	}
 
 }
