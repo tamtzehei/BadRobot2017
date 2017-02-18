73745ebcd0939f762f90b0fa3a1f79713451941a
Dominik Winecki <dominikwinecki@gmail.com>
2017-02-17 18:53:07 -0800

    Remove unused and outdated navx code

--- src/org/usfirst/frc/team1014/robot/subsystems/DriveTrain.java
+++ src/org/usfirst/frc/team1014/robot/subsystems/DriveTrain.java
@@ -14,7 +14,6 @@ public class DriveTrain extends Subsystem{
 	private static DriveTrain instance;
 	SpeedControllerNormalizer normalizer;
 	SwerveWheel wheelA, wheelB, wheelC, wheelD;
-	IMU imu;
 	SerialPort mxpPort;
 	
 	
@@ -24,7 +23,6 @@ public class DriveTrain extends Subsystem{
 			instance = new DriveTrain();
 		return instance;
 	}
-	
 	
 	private DriveTrain()
 	{
@@ -36,11 +34,9 @@ public class DriveTrain extends Subsystem{
 		wheelC = new SwerveWheel(new Vector2d(-1 ,-1), RobotMap.DRIVE_MOTOR_C, RobotMap.PIVOT_MOTOR_C , 824, 882, 13);
 		wheelD = new SwerveWheel(new Vector2d(1 ,-1), RobotMap.DRIVE_MOTOR_D, RobotMap.PIVOT_MOTOR_D, 737, 888, 13);
 		
-		mxpPort = new SerialPort(57600, SerialPort.Port.kMXP);
-		imu = new IMU(mxpPort, (byte) 127);
 		normalizer = new  SpeedControllerNormalizer();
 	}
-    
+
 	public void drive(double rotation, Vector2d translation)
 	{
 		if(Math.abs(rotation) < .15)
@@ -48,12 +44,7 @@ public class DriveTrain extends Subsystem{
 		
 		/*if(translation.magnitude() < .15)
 			translation = new Vector2d(0,0);*/
-	                     
-		double robotAngle = Math.toRadians(imu.getYaw());
-		
-		System.out.println("ANGLE: " + robotAngle);
-		System.out.println("IN: " + translation);
-		
+
 		wheelA.drive(translation, rotation, normalizer);
 		wheelB.drive(translation, rotation, normalizer);
 		wheelC.drive(translation, rotation, normalizer);
