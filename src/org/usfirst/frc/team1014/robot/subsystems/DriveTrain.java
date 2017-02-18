58798355ad3655a4d90c62645671d747e1af9d12
Dominik Winecki <dominikwinecki@gmail.com>
2017-02-17 19:24:48 -0800

    Use lists for swerve wheels

--- src/org/usfirst/frc/team1014/robot/subsystems/DriveTrain.java
+++ src/org/usfirst/frc/team1014/robot/subsystems/DriveTrain.java
@@ -1,5 +1,8 @@
 package org.usfirst.frc.team1014.robot.subsystems;
 
+import java.util.ArrayList;
+import java.util.List;
+
 import org.usfirst.frc.team1014.robot.RobotMap;
 import org.usfirst.frc.team1014.robot.util.Vector2d;
 import org.usfirst.frc.team1014.robot.utils.SpeedControllerNormalizer;
@@ -8,56 +11,49 @@ import org.usfirst.frc.team1014.robot.utils.SwerveWheel;
 import edu.wpi.first.wpilibj.SerialPort;
 import edu.wpi.first.wpilibj.command.Subsystem;
 
-public class DriveTrain extends Subsystem{
-	
-	private static final double L = 1, W = 1;
+public class DriveTrain extends Subsystem {
 	private static DriveTrain instance;
 	SpeedControllerNormalizer normalizer;
-	SwerveWheel wheelA, wheelB, wheelC, wheelD;
+	List<SwerveWheel> swerveWheels;
 	SerialPort mxpPort;
-	
-	
-	public static DriveTrain getInstance()
-	{
-		if(instance == null)
+
+	public static DriveTrain getInstance() {
+		if (instance == null)
 			instance = new DriveTrain();
 		return instance;
 	}
-	
-	private DriveTrain()
-	{
-		//12.75, 11
-		wheelA = new SwerveWheel(new Vector2d(1 ,1), RobotMap.DRIVE_MOTOR_A, RobotMap.PIVOT_MOTOR_A , 354, 888, 13);
-		//wheelB = new SwerveWheel(new Vector2d(-1 ,1), RobotMap.DRIVE_MOTOR_B, RobotMap.PIVOT_MOTOR_B , 737, 867, 13);
 
-		wheelB = new SwerveWheel(new Vector2d(-1 ,1), RobotMap.DRIVE_MOTOR_B, RobotMap.PIVOT_MOTOR_B , 171, 867, 13);
-		wheelC = new SwerveWheel(new Vector2d(-1 ,-1), RobotMap.DRIVE_MOTOR_C, RobotMap.PIVOT_MOTOR_C , 824, 882, 13);
-		wheelD = new SwerveWheel(new Vector2d(1 ,-1), RobotMap.DRIVE_MOTOR_D, RobotMap.PIVOT_MOTOR_D, 737, 888, 13);
-		
-		normalizer = new  SpeedControllerNormalizer();
+	private DriveTrain() {
+		swerveWheels = new ArrayList<SwerveWheel>() {
+			{
+				add(new SwerveWheel(new Vector2d(1, 1), RobotMap.DRIVE_MOTOR_A, RobotMap.PIVOT_MOTOR_A, 354, 888, 13)); // A
+				add(new SwerveWheel(new Vector2d(-1, 1), RobotMap.DRIVE_MOTOR_B, RobotMap.PIVOT_MOTOR_B, 171, 867, 13)); // B
+				add(new SwerveWheel(new Vector2d(-1, -1), RobotMap.DRIVE_MOTOR_C, RobotMap.PIVOT_MOTOR_C, 824, 882, 13)); // C
+				add(new SwerveWheel(new Vector2d(1, -1), RobotMap.DRIVE_MOTOR_D, RobotMap.PIVOT_MOTOR_D, 737, 888, 13)); // D
+			}
+		};
+
+		normalizer = new SpeedControllerNormalizer();
 	}
 
-	public void drive(double rotation, Vector2d translation)
-	{
-		if(Math.abs(rotation) < .15)
+	public void drive(double rotation, Vector2d translation) {
+		if (Math.abs(rotation) < .15)
 			rotation = 0;
-		
-		/*if(translation.magnitude() < .15)
-			translation = new Vector2d(0,0);*/
 
-		wheelA.drive(translation, rotation, normalizer);
-		wheelB.drive(translation, rotation, normalizer);
-		wheelC.drive(translation, rotation, normalizer);
-		wheelD.drive(translation, rotation, normalizer);
-		
-		//normalizer.run();
-		//normalizer.clear();
+		/*
+		 * if(translation.magnitude() < .15) translation = new Vector2d(0,0);
+		 */
+
+		swerveWheels.forEach((w) -> w.drive(translation, rotation, normalizer));
+
+		// normalizer.run();
+		// normalizer.clear();
 	}
 
 	@Override
 	protected void initDefaultCommand() {
 		// TODO Auto-generated method stub
-		
+
 	}
 
 }
