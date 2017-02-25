66279ff8000885b48a4265d45fd3659987eaa028
Dominik Winecki <dominikwinecki@gmail.com>
2017-02-24 21:14:21 -0500

    Remove unused RobotMap constants

--- src/org/usfirst/frc/team1014/robot/RobotMap.java
+++ src/org/usfirst/frc/team1014/robot/RobotMap.java
@@ -8,7 +8,7 @@ package org.usfirst.frc.team1014.robot;
  */
 public class RobotMap {
 	/*
-	 * PWM
+	 * CAN IDs
 	 */
 	public static final int DRIVE_MOTOR_A = 26;
  	public static final int DRIVE_MOTOR_B = 24;
@@ -20,20 +20,5 @@ public class RobotMap {
  	public static final int PIVOT_MOTOR_D = 27;
 	public static final int SHOOTER_MOTOR = 31;
 	public static final int ROTATER_MOTOR = 33;
-	//public static final int CLIMBER_MOTOR = ;
-	
-
-
-	/*
-	 * DIO
-	 */
-	public static final int PIVOT_ENCODER_AA = 9;
-	public static final int PIVOT_ENCODER_AB = 8;
-	public static final int PIVOT_ENCODER_BA = 4;
-	public static final int PIVOT_ENCODER_BB = 3;
-	public static final int PIVOT_ENCODER_CA = 1;
-	public static final int PIVOT_ENCODER_CB = 0;
-	public static final int PIVOT_ENCODER_DA = 6;
-	public static final int PIVOT_ENCODER_DB = 5;
 }
 
