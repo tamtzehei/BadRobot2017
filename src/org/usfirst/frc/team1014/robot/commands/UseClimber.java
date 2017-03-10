ce84818f714323512dfefeb60e8bceb889178e6e
Tze Hei Tam <18tam.tzehei@gmail.com>
2017-03-09 22:49:39 -0500

    Added toggle for shooter and climber

--- src/org/usfirst/frc/team1014/robot/commands/UseClimber.java
+++ src/org/usfirst/frc/team1014/robot/commands/UseClimber.java
@@ -3,18 +3,34 @@ package org.usfirst.frc.team1014.robot.commands;
 import org.usfirst.frc.team1014.robot.OI;
 import org.usfirst.frc.team1014.robot.subsystems.Climber;
 
+import edu.wpi.first.wpilibj.GenericHID.Hand;
 import edu.wpi.first.wpilibj.command.Command;
 
 public class UseClimber extends Command {
 
 	Climber climber;
+	boolean BButton, isClimbing;
 
 	protected void initialize() {
 		climber = Climber.getInstance();
+		BButton = false;
+		isClimbing = false;
 	}
 
 	protected void execute() {
-		climber.climb(OI.xboxController1.getRawAxis(5));
+		if (OI.xboxController1.getBButton())
+			BButton = true;
+		if (!OI.xboxController1.getBButton() && BButton) {
+			BButton = false;
+			isClimbing = !isClimbing;
+		}
+		if (isClimbing) {
+			climber.climb(-1);
+		}
+
+		if (OI.xboxController1.getTriggerAxis(Hand.kRight) > .15 || OI.xboxController1.getTriggerAxis(Hand.kLeft) > .15)
+			climber.climb(
+					OI.xboxController1.getTriggerAxis(Hand.kLeft) - OI.xboxController1.getTriggerAxis(Hand.kRight));
 	}
 
 	@Override
