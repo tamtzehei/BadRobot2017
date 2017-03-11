62f329824d22df1104271adac63245de198156d3
Dominik Winecki <dominikwinecki@gmail.com>
2017-03-10 16:09:58 -0800

    Add power logging

--- src/org/usfirst/frc/team1014/robot/PowerAnalyser.java
+++ src/org/usfirst/frc/team1014/robot/PowerAnalyser.java
@@ -1,24 +1,61 @@
 package org.usfirst.frc.team1014.robot;
 
-import java.util.HashMap;
+import java.io.FileWriter;
+import java.io.IOException;
+import java.util.ArrayList;
+import java.util.LinkedHashMap;
+import java.util.List;
+import java.util.Optional;
 
 import edu.wpi.first.wpilibj.PowerDistributionPanel;
 import edu.wpi.first.wpilibj.smartdashboard.SmartDashboard;
 
 public class PowerAnalyser {
 	PowerDistributionPanel pdp;
-	HashMap<String, Integer> channels;
+	LinkedHashMap<String, Integer> channels;
+	String fileName = "/home/lvuser/powerlog-" + System.currentTimeMillis() / 1000 + ".csv";
+	Optional<FileWriter> fileWriter = Optional.empty();
 
 	public PowerAnalyser(PowerDistributionPanel pdp) {
+		channels = new LinkedHashMap<>();
 		this.pdp = pdp;
+		try {
+			fileWriter = Optional.of(new FileWriter(fileName, true));
+			System.out.println("Writing power to " + fileName);
+		} catch (IOException e) {
+			e.printStackTrace();
+		}
+
 	}
 
 	public void add(String name, int channel) {
-		channels.put(name +"-current", channel);
+		channels.put(name + "-current", channel);
 	}
 
 	public void update() {
 		channels.forEach((k, v) -> SmartDashboard.putDouble(k, pdp.getCurrent(v)));
+		if(!fileWriter.isPresent()) return;
+		Integer[] values = (Integer[]) channels.entrySet().toArray();
+		List<String> valueStrings = new ArrayList<>();
+		for (Integer v : values)
+			valueStrings.add(v.toString());
+		try {
+			fileWriter.get().write(String.join(", ", (String[]) valueStrings.toArray()));
+		} catch (IOException e) {
+			e.printStackTrace();
+		}
+	}
+
+	public void printFieldHeader() {
+		if (!fileWriter.isPresent())
+			return;
+		String[] headers = (String[]) channels.keySet().toArray();
+		try {
+			fileWriter.get().write(String.join(", ", headers));
+		} catch (IOException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
 	}
 
 }
